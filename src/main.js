f277a7917ff72a61a2b9be898e956482d26ff6df
Mathias Eudeline <m.eudeline@crown.fr>
2021-10-19 12:06:07 +0200

    Adjusting main.js for vue 3

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
-import Vuex from 'vuex'
-Vue.config.productionTip = false
 import Vuecode from './vuecode/index.js'
 
-Vue.use(Vuex)
-Vue.use(Vuecode,{
-  // open:true,
-  // theme:'line'
-})
-
-const store = new Vuex.Store({
+const store = createStore({
   state: {
     myName:'Change my text from vuenut'
   },
-})
+});
 
-new Vue({
-  store,
+const app = createApp({
   render: h => h(App)
-}).$mount('#app')
+});
+
+app.use(store);
+app.use(Vuecode, {
+    // open:true,
+    // theme:'line'
+});
+
+app.$mount('#app');
+
+
+
+
